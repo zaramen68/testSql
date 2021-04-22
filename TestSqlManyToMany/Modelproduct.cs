0d4572ab2373de1a599396d9d1dcd3053f714630
zaram <zaramenskiy@awada.ru>
2021-04-22 16:18:00 +0300

    init

---
+++ TestSqlManyToMany/Modelproduct.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace TestSqlManyToMany
+{
+    public class Modelproduct : DbContext
+    {
+        // Контекст настроен для использования строки подключения "Modelproduct" из файла конфигурации  
+        // приложения (App.config или Web.config). По умолчанию эта строка подключения указывает на базу данных 
+        // "TestSqlManyToMany.Modelproduct" в экземпляре LocalDb. 
+        // 
+        // Если требуется выбрать другую базу данных или поставщик базы данных, измените строку подключения "Modelproduct" 
+        // в файле конфигурации приложения.
+        public Modelproduct()
+            : base("name=Modelproduct")
+        {
+        }
+
+        // Добавьте DbSet для каждого типа сущности, который требуется включить в модель. Дополнительные сведения 
+        // о настройке и использовании модели Code First см. в статье http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public ICollection<Category> Categories { get; set; }
+        public Product()
+        {
+            Categories = new List<Category>();
+        }
+    }
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Product> Products { get; set; }
+        public Category()
+        {
+            Products = new List<Product>();
+        }
+    }
+}
---
+++ TestSqlManyToMany/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSqlManyToMany
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Modelproduct db = new Modelproduct())
+            {
+                // создание и добавление моделей
+                Product p1 = new Product { Name = "Шоколад", Price = 31 };
+                Product p2 = new Product { Name = "Кофе", Price = 280 };
+                Product p3 = new Product { Name = "Халва", Price = 74 };
+                db.Products.AddRange(new List<Product> { p1, p2, p3 });
+                db.SaveChanges();
+
+                Category c1 = new Category { Name = "Десерт" };
+                c1.Products.Add(p1);
+                c1.Products.Add(p3);
+                Category c2 = new Category { Name = "Напиток" };
+                c2.Products.Add(p2);
+
+                db.Categories.Add(c1);
+                db.Categories.Add(c2);
+
+                db.SaveChanges();
+                /*
+                foreach (Team t in db.Teams.Include(t => t.Players))
+                {
+                    Console.WriteLine("Команда: {0}", t.Name);
+                    foreach (Player pl in t.Players)
+                    {
+                        Console.WriteLine("{0} - {1}", pl.Name, pl.Position);
+                    }
+                    Console.WriteLine();
+                }
+                */
+            }
+        }
+    }
+}
